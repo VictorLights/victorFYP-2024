afd1dc7665c06218e4bba3e0c246b787904468ee
Victor Lights <victorlights02@gmail.com>
2024-05-20 18:59:35 +0800

    Initial commit

---
+++ AlertActivity.java
@@ -0,0 +1,8 @@
+
+class AlertActivity {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("VS CODE");
+    }
+}
